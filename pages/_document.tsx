358507886e0f82d68b0e8f0b1ce10a50f79d4bfa
Vince <70031472+VinceTheGenius@users.noreply.github.com>
2022-04-12 21:56:00 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,12 +12,8 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.png" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon.png"
-          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.png" />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/favicon.png" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#FFFFFF" />
