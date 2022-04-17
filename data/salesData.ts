ed71cbfff7165f755f7d2d982bb4491e9d793393
Vince <70031472+NitroVince@users.noreply.github.com>
2022-04-17 14:44:49 +0200

    Update salesData.ts

--- data/salesData.ts
+++ data/salesData.ts
@@ -1,7 +1,7 @@
 const salesData = [
     {
       title: 'Discord Bot Website',
-      description: `A perfect website to promote your Discord bot. Contain features, faq and support sections, and a documentation page for the bot's commands.`,
+      description: `A perfect website to promote your Discord bot. There are curves between sections. Contain features, faq and support sections, and a documentation page for the bot's commands.`,
       price: '$10',
       imgSrc: '/static/images/sales/discord-bot-website.png',
       tag: ['html', 'css'],
