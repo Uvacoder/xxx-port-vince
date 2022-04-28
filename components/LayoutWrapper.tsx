0af367f98a6bfd4e1d0ad033715422e7d5dbf5b7
Vince <70031472+VinceTheGenius@users.noreply.github.com>
2022-04-28 10:47:55 +0200

    Update LayoutWrapper.tsx

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -7,6 +7,7 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import { ReactNode } from 'react'
 import { useState, useEffect } from 'react'
+import type {IconType} from 'react-icons';
 import { FiExternalLink } from 'react-icons/fi'
 
 interface Props {
